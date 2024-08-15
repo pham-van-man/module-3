d6dc0e4d553539feff3202a9c95e2ff047659723
pham-van-man <phamvanman.com.vn@gmail.com>
2024-08-15 08:50:58 +0700

    first commit

---
+++ ss1/ss1.sql
@@ -0,0 +1,4 @@
+create database mydatabase;
+use mydatabase;
+create table class (id int auto_increment primary key, name varchar(255));
+create table teacher (id int auto_increment primary key, name varchar(255), age int check(age>18), country varchar(255));
